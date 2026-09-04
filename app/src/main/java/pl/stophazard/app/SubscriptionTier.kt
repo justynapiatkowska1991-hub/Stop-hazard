921d3f0329d1151f8ea01690eba2d8890a805a4a
justynapiatkowska1991-hub <justynapiatkowska1991@gmail.com>
2026-09-04 19:38:01 +0200

    Add subscription tier model

--- app/src/main/java/pl/stophazard/app/SubscriptionTier.kt
+++ app/src/main/java/pl/stophazard/app/SubscriptionTier.kt
@@ -4,9 +4,3 @@ enum class SubscriptionTier {
     BASIC,
     PREMIUM
 }
-
-object Pricing {
-    const val BASIC_MONTHLY_PLN = 19.99
-    const val PREMIUM_MONTHLY_PLN = 29.99
-    const val PREMIUM_YEARLY_PLN = 159.00
-}
