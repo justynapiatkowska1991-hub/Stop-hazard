d00646aaa57dfa8c9a2a9d74918f2841e057e229
justynapiatkowska1991-hub <justynapiatkowska1991@gmail.com>
2026-09-04 19:45:11 +0200

    Add protection event model

--- app/src/main/java/pl/stophazard/app/ProtectionEvent.kt
+++ app/src/main/java/pl/stophazard/app/ProtectionEvent.kt
@@ -1,9 +1,7 @@
 package pl.stophazard.app
 
-sealed class ProtectionEvent {
-    data object ProtectionStarted : ProtectionEvent()
-    data object ProtectionStopped : ProtectionEvent()
-    data object VpnAuthorizationRequired : ProtectionEvent()
-    data class DomainBlocked(val hostname: String) : ProtectionEvent()
-    data class Error(val message: String) : ProtectionEvent()
-}
+data class ProtectionEvent(
+    val host: String,
+    val blocked: Boolean,
+    val timestampMillis: Long = System.currentTimeMillis()
+)
