673b5cbb982e2964b9924ffa5387f75a7c7eb3bc
justynapiatkowska1991-hub <justynapiatkowska1991@gmail.com>
2026-09-05 09:18:55 +0200

    Expand gambling domain blocklist

--- app/src/main/java/pl/stophazard/app/BlockedDomains.kt
+++ app/src/main/java/pl/stophazard/app/BlockedDomains.kt
@@ -5,7 +5,12 @@ object BlockedDomains {
         "bet365.com", "betway.com", "williamhill.com", "888.com",
         "pokerstars.com", "unibet.com", "bwin.com", "ladbrokes.com",
         "coral.co.uk", "skybet.com", "casino.com", "leovegas.com",
-        "betfair.com", "wincasino.io", "win-casino.io", "wincasino.com"
+        "betfair.com", "wincasino.io", "win-casino.io", "wincasino.com",
+        "casino1.com", "tikalcasino.com",
+        "sts.pl", "fortuna.pl", "superbet.pl", "betters.pl",
+        "betfan.pl", "etoto.pl", "lvbet.pl", "forbet.pl",
+        "totalbet.pl", "01bet.com", "22bet.com", "1xbet.com",
+        "melbet.com", "mostbet.com", "betcris.com"
     )
 
     fun domains(): Set<String> = domains
