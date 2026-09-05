19122fab794b050de63ca59145fa692b8cf34f55
justynapiatkowska1991-hub <justynapiatkowska1991@gmail.com>
2026-09-05 07:48:31 +0200

    Fix checksum Int and Long types

--- app/src/main/java/pl/stophazard/app/Ipv4PacketBuilder.kt
+++ app/src/main/java/pl/stophazard/app/Ipv4PacketBuilder.kt
@@ -82,7 +82,7 @@ object Ipv4PacketBuilder {
         }
         if (i < end) sum += (p[i].toInt() and 255) shl 8
         while ((sum ushr 16) != 0L) sum = (sum and 0xffffL) + (sum ushr 16)
-        val result = sum.inv() and 0xffffL
+        val result = (sum.inv() and 0xffffL).toInt()
         return if (result == 0) 0xffff else result
     }
 
@@ -104,7 +104,7 @@ object Ipv4PacketBuilder {
             i += 2
         }
         if (i < end) sum += (p[i].toInt() and 255) shl 8
-        while ((sum ushr 16) != 0) sum = (sum and 0xffff) + (sum ushr 16)
+        while ((sum ushr 16) != 0L) sum = (sum and 0xffffL) + (sum ushr 16)
         return (sum.inv() and 0xffffL).toInt()
     }
 
