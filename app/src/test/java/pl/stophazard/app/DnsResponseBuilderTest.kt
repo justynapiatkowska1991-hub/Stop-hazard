052b1f958eaa4c887b2faa7377a1b2539a7b51ec
justynapiatkowska1991-hub <justynapiatkowska1991@gmail.com>
2026-09-17 22:46:55 +0200

    Test DNS response policy guard

--- app/src/test/java/pl/stophazard/app/DnsResponseBuilderTest.kt
+++ app/src/test/java/pl/stophazard/app/DnsResponseBuilderTest.kt
@@ -8,24 +8,29 @@ import org.junit.Test
 
 class DnsResponseBuilderTest {
     @Test
-    fun buildsNxDomainWithMatchingTransactionIdAndQuestion() {
+    fun blockedDomainGetsNxDomain() {
         val query = query("fortuna.pl")
-        val response = DnsResponseBuilder.nxdomainResponse(query)
+        val response = DnsResponseBuilder.responseFor(query)
 
         assertNotNull(response)
         response!!
         assertEquals(query[0], response[0])
         assertEquals(query[1], response[1])
         val flags = ((response[2].toInt() and 0xff) shl 8) or (response[3].toInt() and 0xff)
-        assertTrue(flags and 0x8000 != 0) // response
-        assertEquals(3, flags and 0x000f) // NXDOMAIN
-        assertEquals(1, response[5].toInt()) // one question
+        assertTrue(flags and 0x8000 != 0)
+        assertEquals(3, flags and 0x000f)
+        assertEquals(1, response[5].toInt())
         assertEquals(query.size, response.size)
     }
 
     @Test
+    fun allowedDomainDoesNotGetBlockedResponse() {
+        assertNull(DnsResponseBuilder.responseFor(query("google.com")))
+    }
+
+    @Test
     fun malformedQueryDoesNotProduceResponse() {
-        assertNull(DnsResponseBuilder.nxdomainResponse(byteArrayOf(1, 2, 3)))
+        assertNull(DnsResponseBuilder.responseFor(byteArrayOf(1, 2, 3)))
     }
 
     private fun query(domain: String): ByteArray {
